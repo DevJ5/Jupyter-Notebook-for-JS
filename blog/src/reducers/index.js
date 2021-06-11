ffe47ad3f4c7336028beba1fe0118773e9ab3833
DevJ5 <idevelopjs@gmail.com>
2021-06-11 19:23:38 +0200

    Reducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,14 +1,5 @@
 import { combineReducers } from 'redux';
-
-const postsReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_POSTS':
-      return action.payload;
-
-    default:
-      return state;
-  }
-};
+import postsReducer from './postsReducer';
 
 export default combineReducers({
   posts: postsReducer,
