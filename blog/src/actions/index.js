47370f16242b2b0cb4e506ef0618421bb10e12c9
DevJ5 <idevelopjs@gmail.com>
2021-06-12 20:42:21 +0200

    redux

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,42 @@
 import jsonPlaceholder from '../api/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => {
-  return async (dispatch, getState) => {
-    const response = await jsonPlaceholder.get('/posts');
-    dispatch({
-      type: 'FETCH_POSTS',
-      payload: response.data,
+  // return async (dispatch, getState) => {
+  //   const response = await jsonPlaceholder.get('/posts');
+  //   dispatch({
+  //     type: 'FETCH_POSTS',
+  //     payload: response.data,
+  //   });
+  // };
+
+  return (dispatch, getState) => {
+    jsonPlaceholder.get('/posts').then((response) => {
+      dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data,
+      });
     });
   };
 };
+
+// export const fetchUser = (id) => async (dispatch, getState) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   console.log(response.data);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data,
+//   });
+// };
+
+export const fetchUser = (id) => (dispatch, getState) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data,
+  });
+});
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 export class PostList extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.props.posts.map((post) => (
-            <li>{post.title}</li>
-          ))}
-        </ul>
+  renderList() {
+    return this.props.posts.map((post) => (
+      <div className="item" key={post.id}>
+        <i className="large middle aligned icon user"></i>
+        <div className="content">
+          <div className="description">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        </div>
+        <UserHeader userId={post.userId} />
       </div>
-    );
+    ));
+  }
+
+  render() {
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
