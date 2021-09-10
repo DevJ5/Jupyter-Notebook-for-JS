f40332a2848d389c0cd9aecbf4138fcc6047f380
DevJ5 <idevelopjs@gmail.com>
2021-09-10 14:26:59 +0200

    Client queries

--- graphql/client/src/pages/LandingPage.js
+++ graphql/client/src/pages/LandingPage.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import MainHero from "../components/MainHero/MainHero"
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
-import CardDisplay from "../components/CardDisplay/CardDisplay"
+import React from 'react';
+import MainHero from '../components/MainHero/MainHero';
+import CategoryDisplay from '../components/CategoryDisplay/CategoryDisplay';
+import CardDisplay from '../components/CardDisplay/CardDisplay';
+import { useQuery, gql } from '@apollo/client';
+
+const FETCH_ANIMALS = gql`
+  {
+    animals {
+      id
+      image
+      price
+      slug
+      image
+    }
+  }
+`;
 
 function LandingPage() {
+  const { loading, error, data } = useQuery(FETCH_ANIMALS);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
 
-    return (
-        <div>
-            <MainHero />
-            <CategoryDisplay />
-            <CardDisplay animals={[]}/>
-        </div>
-    )
+  return (
+    <div>
+      <MainHero />
+      <CategoryDisplay />
+      <CardDisplay animals={data.animals} />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
