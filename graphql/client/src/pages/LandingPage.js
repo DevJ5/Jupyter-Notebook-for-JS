cc07ce80eaec37e51abe799712bb0fcec559871c
DevJ5 <idevelopjs@gmail.com>
2021-09-10 15:32:38 +0200

    Mutations

--- graphql/client/src/pages/LandingPage.js
+++ graphql/client/src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MainHero from '../components/MainHero/MainHero';
 import CategoryDisplay from '../components/CategoryDisplay/CategoryDisplay';
 import CardDisplay from '../components/CardDisplay/CardDisplay';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const FETCH_ANIMALS = gql`
   {
@@ -16,16 +16,57 @@ const FETCH_ANIMALS = gql`
   }
 `;
 
+const ADD_ANIMAL_MUTATION = gql`
+  mutation (
+    $image: String!
+    $category: String!
+    $title: String!
+    $price: String!
+    $description: [String!]!
+    $rating: Float
+    $slug: String!
+  ) {
+    addAnimal(
+      image: $image
+      category: $category
+      title: $title
+      price: $price
+      description: $description
+      rating: $rating
+      slug: $slug
+    ) {
+      id
+    }
+  }
+`;
+
 function LandingPage() {
   const { loading, error, data } = useQuery(FETCH_ANIMALS);
+  const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION);
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error...</div>;
+
+  const addOstrichHandler = () => {
+    addAnimal({
+      variables: {
+        image: 'ostrich',
+        category: '1',
+        title: 'This is a really cool ostrich',
+        price: '32,333',
+        description: ['das'],
+        rating: 3.5,
+        slug: 'ostrich',
+      },
+    });
+  };
 
   return (
     <div>
       <MainHero />
       <CategoryDisplay />
       <CardDisplay animals={data.animals} />
+      <button onClick={addOstrichHandler}>Add an Ostrich</button>
     </div>
   );
 }
