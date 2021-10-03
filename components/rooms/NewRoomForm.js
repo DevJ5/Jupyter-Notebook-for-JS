cfa3d2843f8e5c111c6ed69d0173c95ee182a509
DevJ5 <idevelopjs@gmail.com>
2021-10-03 11:35:18 +0200

    Fixed form css

--- components/rooms/NewRoomForm.js
+++ components/rooms/NewRoomForm.js
@@ -28,33 +28,31 @@ function NewRoomForm(props) {
   }
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="title">Room Title</label>
-          <input type="text" required id="title" ref={titleInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="image">Room Image</label>
-          <input type="url" required id="image" ref={imageInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="address">Address</label>
-          <input type="text" required id="address" ref={addressInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            required
-            rows="5"
-            ref={descriptionInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button type="submit">Add Room</button>
-        </div>
-      </form>
-    </Card>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="title">Room Title</label>
+        <input type="text" required id="title" ref={titleInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="image">Room Image</label>
+        <input type="url" required id="image" ref={imageInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="address">Address</label>
+        <input type="text" required id="address" ref={addressInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          required
+          rows="5"
+          ref={descriptionInputRef}></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button type="submit">Add Room</button>
+      </div>
+    </form>
   );
 }
 
